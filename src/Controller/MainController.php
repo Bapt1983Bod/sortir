5387a77481e919053d09776403f95a28b5dee814
ThomasKoz <thomas.kozlowski86@gmail.com>
2024-04-02 16:41:46 +0200

    Main page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+
 
 class MainController extends AbstractController
 {
     #[Route('/main', name: 'app_main')]
-    public function index(): Response
+    public function index(Request $request, SortieRepository $sortieRepository): Response
     {
+
+        // Récupérer les sorties depuis la base de données
+        $sorties = $sortieRepository->findAll();
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'sorties' => $sorties,
         ]);
     }
 }
