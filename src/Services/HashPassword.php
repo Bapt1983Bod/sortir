4c3ec54c541fd23bb06a4afb3dc7e495721e6d44
Bapt1983Bod <152866910+Bapt1983Bod@users.noreply.github.com>
2024-04-04 12:02:57 +0200

    services

--- src/Services/HashPassword.php
+++ src/Services/HashPassword.php
@@ -7,7 +7,7 @@ use App\Entity\Participant;
 class HashPassword
 {
 
-    public function hashPassword (Participant $user, $passwordToHash)
+    public function hashPassword (Participant $user, $passwordToHash) : void
     {
         $hashedPassword = password_hash($passwordToHash,PASSWORD_BCRYPT);
         $user->setPassword($hashedPassword);
