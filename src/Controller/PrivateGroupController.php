ba188270a9b886dbefd6c5362883c9dfc2597afb
ThomasKoz <thomas.kozlowski86@gmail.com>
2024-04-10 10:37:01 +0200

    Améliorations groupes privés

--- src/Controller/PrivateGroupController.php
+++ src/Controller/PrivateGroupController.php
@@ -11,26 +11,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Security;
 
 class PrivateGroupController extends AbstractController
 {
     #[Route('/group', name: 'app_group')]
     public function index(GroupRepository $groupRepository): Response
     {
+
+        $user=$this->getUser();
         $groups = $groupRepository->findAll();
         return $this->render('private_group/index.html.twig', [
             'groups' => $groups,
+            'user'=>$user,
         ]);
     }
 
 #[Route('/group/create', name: 'app_group_create')]
-    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    public function create(Request $request, EntityManagerInterface $entityManager, Security $security): Response
 {
     $group = new Group();
     $form = $this->createForm(GroupFormType::class, $group);
     $form->handleRequest($request);
 
     if ($form->isSubmitted() && $form->isValid()) {
+//        faire en sorte que le créateur soit automatiquement inscrit dans le groupe
+        $user=$security->getUser();
+        $group->addMembre($user);
+
         $entityManager->persist($group);
         $entityManager->flush();
 
