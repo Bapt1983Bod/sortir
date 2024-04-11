8cd73276fc2615b8fa286a3dc25a4af5e578fdc0
Bapt1983Bod <152866910+Bapt1983Bod@users.noreply.github.com>
2024-04-11 14:15:53 +0200

    maj affichage formulaire lieu

--- src/Form/LieuType.php
+++ src/Form/LieuType.php
@@ -7,6 +7,7 @@ use App\Entity\Ville;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,13 +16,17 @@ class LieuType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('rue')
+            ->add('nom', TextType::class,[
+                'attr' => ['class'=> 'form-control']
+            ])
+            ->add('rue', TextType::class,[
+                'attr' => ['class'=> 'form-control']
+            ])
             ->add('ville', EntityType::class, [
                 'class' => Ville::class,
                 'choice_label' => 'nom',
+                'attr'=>['class'=> 'form-control']
             ])
-            ->add('submit', SubmitType::class)
         ;
     }
 
